b8737f5a65b577e8ffb86ce2dbd1c26bed72b01f
OssianN <ossian.northen@gmail.com>
2024-08-08 13:54:33 +0200

    implements server fetch

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,28 +1,10 @@
-//
-
-import { SearchResultResponse } from '@/types';
+import { getMovies } from '@/utils/getMovie';
 import { NextRequest } from 'next/server';
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
-  const apiKey = process.env.OMDB_API_KEY;
 
-  if (!apiKey) {
-    return Response.json({ error: 'Missing API key' });
-  }
-
-  if (typeof body?.searchText !== 'string') {
-    return Response.json({ error: 'body must be of type string' });
-  }
-
-  const encodedSearch = encodeURIComponent(body?.searchText);
-
-  const response = await fetch(
-    `http://www.omdbapi.com/?apikey=${apiKey}&t=${encodedSearch}&plot=full`
-  );
-
-  const data: SearchResultResponse = await response.json();
-  console.log(data);
+  const data = await getMovies(body?.searchText);
 
   return Response.json(data);
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import { Search } from '@/components/Search';
+import { getMovies } from '@/utils/getMovie';
 
-const Home = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Search />
-    </main>
-  );
+type HomeProps = {
+  searchParams: { searchText: string };
+};
+const Home = async ({ searchParams: { searchText } }: HomeProps) => {
+  try {
+    const searchResult = await getMovies(searchText);
+
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Search serverSearchResult={searchResult} />
+      </main>
+    );
+  } catch {}
 };
 
 export default Home;
