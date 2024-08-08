8f7a3ee66fd9d0bd7b343e94d3c7ffd9c3982111
OssianN <ossian.northen@gmail.com>
2024-08-08 12:53:32 +0200

    adds types

---
+++ types.ts
@@ -0,0 +1,40 @@
+import { z } from 'zod';
+
+export const formSchema = z.object({
+  searchInput: z.string().min(2).max(50),
+});
+
+export type FormType = z.infer<typeof formSchema>;
+
+type SearchResultRating = {
+  Source: string;
+  Value: string;
+};
+
+export type SearchResultResponse = {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: SearchResultRating[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string;
+  BoxOffice: string;
+  Production: string;
+  Website: string;
+  Response: string;
+};
